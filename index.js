bff6592342c2e17a5116928431766fd48ffc8ab0
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-12 14:34:19 +0500

    send server html page

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.end("<h1> Page</h1>");
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  } else if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 });
 
 const PORT = process.env.PORT || 4000;
