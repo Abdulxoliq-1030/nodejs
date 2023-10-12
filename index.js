d1d3b3eb707758ee6bcc65b0c9a1bbc1dd06e16a
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-12 11:34:51 +0500

    http module second part

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Logger = require("./logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const logger = new Logger();
-
-logger.on("message", (data) => {
-  console.log(`Logging`, data);
+const server = http.createServer((req, res) => {
+  res.end("<h1> Page</h1>");
 });
 
-logger.log("GET", "/admin/dashboard");
-logger.log("POST", "/product/add");
-logger.log("DELETE", "/users/23456");
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
